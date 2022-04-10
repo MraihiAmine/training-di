c1fac762d68a24e5b6f8fd2d158994815e2e1913
Mraihi Amine <mraihiamin@gmail.com>
2022-04-10 11:36:57 +0100

    main branch

---
+++ src/main/java/com/springframework/di/trainingdi/TrainingDiApplication.java
@@ -0,0 +1,14 @@
+package com.springframework.di.trainingdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingDiApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
